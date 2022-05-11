a7d49ea5d33e8ec8bd053f4b4431eb7b1a2d05cb
lana <lana@DESKTOP-6HRKR5O>
2022-05-11 18:42:08 +0300

    test lanaa aaaaaaa

--- cpit_251/src/cpit_251/Cpit_251.java
+++ cpit_251/src/cpit_251/Cpit_251.java
@@ -15,7 +15,7 @@ public class Cpit_251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("tt");
+        System.out.println("ttoo");
         // TODO code application logic here
     }
     
