d268af1889b73825feb85dad5f143cafba78b3e4
duaas <duaas@LAPTOP-7Q32PDUK>
2022-05-11 19:16:36 +0300

    duaa test push

--- cpit_251/src/cpit_251/Cpit_251.java
+++ cpit_251/src/cpit_251/Cpit_251.java
@@ -15,7 +15,7 @@ public class Cpit_251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ttoo");
+        System.out.println("ttoo diaa");
         // TODO code application logic here
     }
     
