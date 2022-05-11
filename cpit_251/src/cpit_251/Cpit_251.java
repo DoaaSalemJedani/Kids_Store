0de4eb0283f06bdb87b142c52378d2f5c6be2747
Sara <Sara@LAPTOP-NHFDEQG8>
2022-05-11 19:20:22 +0300

    test sara

--- cpit_251/src/cpit_251/Cpit_251.java
+++ cpit_251/src/cpit_251/Cpit_251.java
@@ -15,8 +15,8 @@ public class Cpit_251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ttoo diaa");
-        // TODO code application logic here
+        System.out.println("ttoo diaa sara");
+       
     }
     
 }
