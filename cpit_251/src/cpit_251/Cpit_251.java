ce78e963e53566d1cb28197ff031ad3e4c9e3dc0
Sara <Sara@LAPTOP-NHFDEQG8>
2022-05-11 20:16:32 +0300

    last test

--- cpit_251/src/cpit_251/Cpit_251.java
+++ cpit_251/src/cpit_251/Cpit_251.java
@@ -15,8 +15,7 @@ public class Cpit_251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ttoo diaa sara");
-       
+        System.out.println("ttoo diaa sara.......");     
     }
     
 }
