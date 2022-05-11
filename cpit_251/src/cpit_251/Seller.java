c6dea3a9b059d87921f29588ac5266f1931decca
duaas <duaas@LAPTOP-7Q32PDUK>
2022-05-11 20:28:22 +0300

    Add item to store method in Seller class Doaa Salem

--- cpit_251/src/cpit_251/Seller.java
+++ cpit_251/src/cpit_251/Seller.java
@@ -9,10 +9,18 @@ package cpit_251;
  *
  * @author Sara
  */
+import java.util.*;
+
+
 public class Seller extends User {
+
+  
+    
 
     public String StoreName;
     public int RegistrationNumber;
+    public int Item_Id ;
+    public int price_item;
 
     public Seller(int Phone, char password, String FirstName, String LastName) {
         super(Phone, password, FirstName, LastName);
@@ -50,10 +58,22 @@ public class Seller extends User {
         this.LastName = LastName;
     }
 
-    public void AddItemToStore(String Item) {
+    public void AddItemToStore(  int quantity , String itemName ) {
+       int addItemLimt=100;
+        
+        for ( int i=1;i<=addItemLimt; i++){
+            
+            Item_Id=i;//to string for store name 
+            Scanner input= new Scanner (System.in);
+            System.out.println(" set the price for  this id number "+ Item_Id + ":" );
+            price_item = input.nextInt();
+            System.out.println(" set the quantity  for  this id number "+ Item_Id + ":" );
+            quantity = input.nextInt();  
+            System.out.println(" set the item name   for  this id number "+ Item_Id + ":" );
+            itemName = input.nextLine();  
+        }
 
     }
-
     public void CheckReview() { //not sure
 
     }
