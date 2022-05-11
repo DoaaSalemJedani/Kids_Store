da2abbbfbb5f5667c8a93e1da811bbb3e895ee80
lana <lana@DESKTOP-6HRKR5O>
2022-05-11 21:13:06 +0300

    lana

--- cpit_251/src/cpit_251/Cpit_251.java
+++ cpit_251/src/cpit_251/Cpit_251.java
@@ -15,6 +15,7 @@ public class Cpit_251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("uu");
        
         
     }
