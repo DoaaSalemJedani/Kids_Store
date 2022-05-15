c2de7ed861da5fef1ef5455a7b4f08ebd9506d3c
lana <lana@DESKTOP-6HRKR5O>
2022-05-15 22:54:36 +0300

    do the change to the (add item to store) its ready lana

--- cpit_251/src/cpit_251/Seller.java
+++ cpit_251/src/cpit_251/Seller.java
@@ -9,23 +9,38 @@ package cpit_251;
  *
  * @author Sara
  */
-import java.util.*;
 
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author duaas
+ */
 public class Seller extends User {
 
-  
-    
+     public static String StoreName;
+    public static String Item_Id;
+    public static int addItemLimt = 50;
+    public static double price_item;
+    public static String ProductType;
+    public static String itemName;
+    public static String item_remaining;
 
-    public String StoreName;
-    public int RegistrationNumber;
-    public int Item_Id ;
-    public int price_item;
-
-    public Seller(String StoreName, String Phone, char password, String FirstName, String LastName) {
+    public Seller(String StoreName, int RegistrationNumber, int Item_Id, int addItemLimt, int girlLimit, int boyLimit, int unisexLimit, String Phone, char password, String FirstName, String LastName) {
         super(Phone, password, FirstName, LastName);
         this.StoreName = StoreName;
+        this.addItemLimt = addItemLimt;
+
     }
+
 
 
     public String getStoreName() {
@@ -36,13 +51,7 @@ public class Seller extends User {
         this.StoreName = StoreName;
     }
 
-    public int getRegistrationNumber() {
-        return RegistrationNumber;
-    }
 
-    public void setRegistrationNumber(int RegistrationNumber) {
-        this.RegistrationNumber = RegistrationNumber;
-    }
 
     public String getFirstName() {
         return FirstName;
@@ -59,40 +68,117 @@ public class Seller extends User {
     public void setLastName(String LastName) {
         this.LastName = LastName;
     }
+        public void updateStore()
+    {
+        try {
+                FileWriter myWriter = new FileWriter("products.txt", true);
+                myWriter.write(StoreName + "," + Item_Id + "," + ProductType + "," + itemName + "," + price_item + "SAR" +"\n");
+                myWriter.flush();
+                myWriter.close();
 
-    public void AddItemToStore( int addItemLimt ) {   
-        
-        for (int i = 1; i <= addItemLimt; i++) {
+                FileReader Reader = new FileReader("products.txt");
 
-            int Item_Id = i;//to string for store name
-
-            Scanner input = new Scanner(System.in);
-
-            System.out.println(" set the name for this item number " + Item_Id + ":");
-            String itemName = input.nextLine();
-
-            //System.out.println("Wrong value entered ! Try again");
-            //itemName = input.nextLine();
-            //System.exit(0);
-            System.out.println(" set the price for this item number " + Item_Id + ":");
-            double price_item = input.nextInt();
-
-            // System.out.println("Wrong value entered ! Try again");
-            // price_item = input.nextInt();
-            // System.exit(0);
-            System.out.println(" set the quantity for this item number " + Item_Id + ":");
-            int quantity = input.nextInt();
-
-            // System.out.println("Wrong value entered ! Try again");
-            // quantity = input.nextInt();
-            //System.exit(0);
-        }
-
+                Reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
     
 
-    }
-    public void CheckReview() { //not sure
 
+   public void AddItemToStore() throws FileNotFoundException, IOException { 
+
+ 
+            File myObj = new File("products.txt");
+   
+
+        Scanner INPUTStoreName = new Scanner(System.in);
+        System.out.print("\nEnter your store name: ");
+        StoreName = INPUTStoreName.nextLine();
+            System.out.println("The limit for adding products is 50.");
+        for (int i = 1; i <= addItemLimt; i++) {
+
+            Item_Id = i + StoreName;//join the store name and id for uniqe id  
+             
+            while (addItemLimt==50) {
+                Scanner INPUT = new Scanner(System.in);
+
+                try {
+
+                    System.out.print("\nSet the name for item number " + Item_Id + " : ");
+                    itemName = INPUT.nextLine().toLowerCase();
+
+                    if (itemName.matches("^[a-z][a-z ]*[a-z]?$")) {
+                        break;
+                    }
+
+                } catch (InputMismatchException E) {
+
+                    INPUT.nextLine();
+                    continue;
+
+                }
+                System.out.println("*** Invalid input: Enter A string from ( A-Z ,a-z ) *** ");
+            }
+
+            while (true) {
+                Scanner INPUT_price = new Scanner(System.in);
+
+                try {
+
+                    System.out.print("Set the price for item number " + Item_Id + " : ");
+                    price_item = INPUT_price.nextDouble();
+                    break;
+
+                } catch (InputMismatchException E) {
+                    System.out.println("*** Invalid input: Enter An integer number *** ");
+
+                    INPUT_price.hasNextDouble();
+                    continue;
+                }
+
+            }
+
+            while (true) {
+                Scanner INPUT_type = new Scanner(System.in);
+
+                try {
+                    System.out.println("Set the Product Type for item number " + Item_Id + " : "
+                            + "girls gifts = g | "
+                            + "boys gifts = b | "
+                            + "unisex gifts = u ");
+                    ProductType = INPUT_type.nextLine().toLowerCase();
+
+                    if (ProductType.equals("b") || ProductType.equals("u") || ProductType.equals("g")) {
+                        break;
+                    }
+
+                } catch (InputMismatchException E) {
+                    INPUT_type.hasNext();
+                    continue;
+                }
+                System.out.println("*** Invalid input , try again *** ");
+
+            }
+
+
+            updateStore();
+            Scanner INPUT_final = new Scanner(System.in);
+
+            System.out.print("\nEnter (E) for end  if no item is remaining , if else press (ENTER): ");
+            item_remaining = INPUT_final.nextLine().toLowerCase();
+
+            if (item_remaining.equals("e")) {
+                break;
+            } else {
+                continue;
+            }
+        }
     }
+
+    
+    
+    
+
 
 }
